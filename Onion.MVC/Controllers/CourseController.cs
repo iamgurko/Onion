af207cc24a12f1b8bb6528ef1610df821c504745
Gürkan Aydoğan <gurkanaydogan@outlook.com.tr>
2019-08-22 00:23:39 +0300

    Commit for Presentation Layer Controller, View, Styling and Image

---
+++ Onion.MVC/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Onion.Application.Interfaces;
+using Onion.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Onion.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        // GET:/<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
