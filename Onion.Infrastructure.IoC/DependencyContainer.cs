928bf8cb617f5d8c09f46e0991d0ea56bba15e36
Gürkan Aydoğan <gurkanaydogan@outlook.com.tr>
2019-08-21 23:46:57 +0300

    Presentation Layer Configration for Dependency Container

---
+++ Onion.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Onion.Application.Interfaces;
+using Onion.Application.Services;
+using Onion.Domain.Interfaces;
+using Onion.Infrastructure.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Onion.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection services)
+        {
+            //Application Layer
+            services.AddScoped<ICourseService, CourseService>();
+            //Infrastructure Data Layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+
+        }
+    }
+}
--- Onion.MVC/Startup.cs
+++ Onion.MVC/Startup.cs
@@ -14,6 +14,7 @@ using Onion.MVC.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Onion.Infrastructure.Data.Context;
+using Onion.Infrastructure.IoC;
 
 namespace Onion.MVC
 {
@@ -49,6 +50,7 @@ namespace Onion.MVC
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -78,6 +80,11 @@ namespace Onion.MVC
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterService(services);
+
         }
     }
 }
